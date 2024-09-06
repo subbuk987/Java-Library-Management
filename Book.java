a68169799e0d50d770dd25124f861d11d315e8e8
Subramanyam Kandlagunta <subramanyamk2003@gmail.com>
2024-09-06 20:14:31 +0530

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,28 @@
+public class Book {
+    private String title;
+    private String author;
+    private int id;
+
+    public Book(String title, String author, int id) {
+        this.title = title;
+        this.author = author;
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Book ID: " + id + ", Title: " + title + ", Author: " + author;
+    }
+}
